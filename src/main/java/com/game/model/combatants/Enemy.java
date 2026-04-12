deabd87bc159747035f4ff89aab8ab825b86c2aa
DanialAidil <danialaidilazhar@gmail.com>
2026-04-13 00:33:27 +0800

    Updated actions in combatants, removed stubs

--- src/main/java/com/game/model/combatants/Enemy.java
+++ src/main/java/com/game/model/combatants/Enemy.java
@@ -1,5 +1,6 @@
 package com.game.model.combatants;
  
+import com.game.actions.BasicAttack;
 import com.game.model.core.Action;
 import com.game.model.core.CombatStats;
 import com.game.model.core.Combatant;
@@ -13,20 +14,8 @@ public abstract class Enemy extends Combatant {
  
 
     public Action chooseAction() {
-        // TODO: replace stub with BasicAttack
-        // return new BasicAttack();
-        return new Action() {
-            @Override
-            public void execute(Combatant actor, Combatant target,
-                                List<Combatant> allEnemies) {
-                if (target == null) return;
-                int damage = Math.max(0, actor.getAttack() - target.getDefense());
-                target.takeDamage(damage);
-            }
-            @Override
-            public String getActionName() { return "Basic Attack"; }
-        };
-    }
+    return new BasicAttack();
+    }   
 
     public Combatant chooseTarget(List<Combatant> validTargets) {
         if (validTargets == null) throw new NullPointerException("validTargets must not be null");
